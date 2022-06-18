a96da076839f4936b13bd848471331e2958c76ce
Chad Smith <chad.smith@vanderbilt.edu>
2022-06-17 22:44:32 -0400

    initial commit

---
+++ Develop/index.js
@@ -0,0 +1,30 @@
+// packages needed for this application
+const inquirer = require('inquirer');
+
+
+// TODO: Create an array of questions for user input
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?',
+        validate: titleInput => {
+            if (titleInput) {
+              return true;
+            } else {
+              console.log('Please enter the title of your project!');
+              return false;
+            }
+          }
+      },
+
+]
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
